00228af78e092aee12a8b04af498e8f12cadf2a0
hennigo <hennigo@w1.ai>
2016-04-27 18:17:48 -0400

    little more

--- ring_net.py
+++ ring_net.py
@@ -77,60 +77,59 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
     tf.add_to_collection('losses', weight_decay)
   return var
 
+def _conv_layer(inputs, kernel_size, stride, num_features, idx):
+  with tf.variable_scope('{0}_conv'.format(idx)) as scope:
+    input_channels = inputs.get_shape()[3]
+
+    weights = _variable_with_weight_decay('weights', shape=[kernel_size,kernel_size,input_channels,num_features],stddev=0.1, wd=FLAGS.weight_decay)
+    biases = _variable_on_cpu('biases',[num_features],tf.constant_initializer(0.1))
+
+    conv = tf.nn.conv2d(inputs, weights, strides=[1, stride, stride, 1], padding='SAME')
+    conv_biased = tf.nn.bias_add(conv, biases)
+    #Leaky ReLU
+    conv_rect = tf.maximum(FLAGS.alpha*conv_biased,conv_biased,name='{0}_conv'.format(idx))
+    return conv_rect
+
+def _transpose_conv_layer(inputs, kernel_size, stride, num_features, idx):
+  with tf.variable_scope('{0}_trans_conv'.format(idx)) as scope:
+    input_channels = inputs.get_shape()[3]
+    
+    weights = _variable_with_weight_decay('weights', shape=[kernel_size,kernel_size,num_features,input_channels], stddev=0.1, wd=FLAGS.weight_decay)
+    biases = _variable_on_cpu('biases',[num_features],tf.constant_initializer(0.1))
+    batch_size = tf.shape(inputs)[0]
+    output_shape = tf.pack([tf.shape(inputs)[0], tf.shape(inputs)[1]*stride, tf.shape(inputs)[2]*stride, num_features]) 
+    conv = tf.nn.conv2d_transpose(inputs, weights, output_shape, strides=[1,stride,stride,1], padding='SAME')
+    conv_biased = tf.nn.bias_add(conv, biases)
+    #Leaky ReLU
+    conv_rect = tf.maximum(FLAGS.alpha*conv_biased,conv_biased,name='{0}_transpose_conv'.format(idx))
+    return conv_rect
+     
+
+def _fc_layer(inputs, hiddens, idx, flat = False, linear = False):
+  with tf.variable_scope('fc{0}'.format(idx)) as scope:
+    input_shape = inputs.get_shape().as_list()
+    if flat:
+      dim = input_shape[1]*input_shape[2]*input_shape[3]
+      inputs_processed = tf.reshape(inputs, [-1,dim])
+    else:
+      dim = input_shape[1]
+      inputs_processed = inputs
+    
+    weights = _variable_with_weight_decay('weights', shape=[dim,hiddens],stddev=0.01, wd=FLAGS.weight_decay)
+    biases = _variable_on_cpu('biases', [hiddens], tf.constant_initializer(0.01))
+    if linear:
+      return tf.add(tf.matmul(inputs_processed,weights),biases,name=str(idx)+'_fc')
+  
+    ip = tf.add(tf.matmul(inputs_processed,weights),biases)
+    return tf.maximum(FLAGS.alpha*ip,ip,name=str(idx)+'_fc')
+
+
 
 def inference(images):
   """Build the ring network for 28x28 dynamical systems.
   Args:
     inputs: input not sure yet
   """
-  def conv_layer(inputs, kernel_size, stride, num_features, idx):
-    with tf.variable_scope('{0}_conv'.format(idx)) as scope:
-      input_channels = inputs.get_shape()[3]
-
-      weights = _variable_with_weight_decay('weights', shape=[kernel_size,kernel_size,input_channels,num_features],stddev=0.1, wd=FLAGS.weight_decay)
-      biases = _variable_on_cpu('biases',[num_features],tf.constant_initializer(0.1))
-
-      conv = tf.nn.conv2d(inputs, weights, strides=[1, stride, stride, 1], padding='SAME')
-      conv_biased = tf.nn.bias_add(conv, biases)
-      #Leaky ReLU
-      conv_rect = tf.maximum(FLAGS.alpha*conv_biased,conv_biased,name='{0}_conv'.format(idx))
-      return conv_rect
-  
-  def transpose_conv_layer(inputs, kernel_size, stride, num_features, idx):
-    with tf.variable_scope('{0}_trans_conv'.format(idx)) as scope:
-      input_channels = inputs.get_shape()[3]
-      
-      weights = _variable_with_weight_decay('weights', shape=[kernel_size,kernel_size,num_features,input_channels], stddev=0.1, wd=FLAGS.weight_decay)
-      biases = _variable_on_cpu('biases',[num_features],tf.constant_initializer(0.1))
-      batch_size = tf.shape(x_1_image)[0]
-      output_shape = tf.pack([tf.shape(inputs)[0], tf.shape(inputs)[1]*stride, tf.shape(inputs)[2]*stride, num_features]) 
-      conv = tf.nn.conv2d_transpose(inputs, weights, output_shape, strides=[1,stride,stride,1], padding='SAME')
-      conv_biased = tf.nn.bias_add(conv, biases)
-      #Leaky ReLU
-      conv_rect = tf.maximum(FLAGS.alpha*conv_biased,conv_biased,name='{0}_transpose_conv'.format(idx))
-      return conv_rect
-       
-  
-  def fc_layer(inputs, hiddens, idx, flat = False, linear = False):
-    with tf.variable_scope('fc{0}'.format(idx)) as scope:
-      input_shape = inputs.get_shape().as_list()
-      if flat:
-        dim = input_shape[1]*input_shape[2]*input_shape[3]
-        inputs_processed = tf.reshape(inputs, [-1,dim])
-      else:
-        dim = input_shape[1]
-        inputs_processed = inputs
-      
-      weights = _variable_with_weight_decay('weights', shape=[dim,hiddens],stddev=0.01, wd=FLAGS.weight_decay)
-      biases = _variable_on_cpu('biases', [hiddens], tf.constant_initializer(0.01))
-      if linear:
-        return tf.add(tf.matmul(inputs_processed,weights),biases,name=str(idx)+'_fc')
-    
-      ip = tf.add(tf.matmul(inputs_processed,weights),biases)
-      return tf.maximum(FLAGS.alpha*ip,ip,name=str(idx)+'_fc')
-
-  #--------- Making the net -----------
-  # x_1 -> y_1 -> y_2 -> x_2
   x_1 = images
   #x_1 = tf.placeholder(tf.float32, [None, 784])
   #x_2 = tf.placeholder(tf.float32, [None, 784])
@@ -139,31 +138,31 @@ def inference(images):
  
   # normalize and formate the first layer
   #keep_prob = tf.placeholder("float") # do a little dropout to normalize
-  x_1_norm = tf.nn.dropout(x_1, .08)
+  x_1_norm = tf.nn.dropout(x_1, .8)
   x_1_image = tf.reshape(x_1_norm, [-1, 28, 28, 1])
   # Need the batch size for the transpose layers.
   batch_size = tf.shape(x_1_image)[0]
  
   # conv1
-  conv1 = conv_layer(x_1_image, 5, 1, 32, 1)
+  conv1 = _conv_layer(x_1_image, 5, 1, 32, 1)
   # conv2
-  conv2 = conv_layer(conv1, 2, 2, 32, 2)
+  conv2 = _conv_layer(conv1, 2, 2, 32, 2)
   # conv3
-  conv3 = conv_layer(conv2, 5, 1, 64, 3)
+  conv3 = _conv_layer(conv2, 5, 1, 64, 3)
   # conv4
-  conv4 = conv_layer(conv3, 2, 2, 64, 4)
+  conv4 = _conv_layer(conv3, 2, 2, 64, 4)
   # conv5
-  conv5 = conv_layer(conv4, 1, 1, 5, 5)
+  conv5 = _conv_layer(conv4, 1, 1, 5, 5)
   # conv6
-  conv6 = transpose_conv_layer(conv5, 1, 1, 64, 6)
+  conv6 = _transpose_conv_layer(conv5, 1, 1, 64, 6)
   # conv7
-  conv7 = transpose_conv_layer(conv6, 2, 2, 64, 7)
+  conv7 = _transpose_conv_layer(conv6, 2, 2, 64, 7)
   # conv8
-  conv8 = transpose_conv_layer(conv7, 5, 1, 32, 8)
+  conv8 = _transpose_conv_layer(conv7, 5, 1, 32, 8)
   # conv9 
-  conv9 = transpose_conv_layer(conv8, 2, 2, 32, 9)
+  conv9 = _transpose_conv_layer(conv8, 2, 2, 32, 9)
   # conv10 
-  conv10 = transpose_conv_layer(conv9, 5, 1, 1, 10)
+  conv10 = _transpose_conv_layer(conv9, 5, 1, 1, 10)
   # reshape
   
   _x_2 = tf.reshape(conv10, [-1, 784])
@@ -173,13 +172,88 @@ def inference(images):
   #accuracy = tf.nn.l2_loss(x_2 - _x_2)
   return _x_2
 
+def encoding(inputs):
+  """Builds encoding part of ring net.
+  Args:
+    inputs: input to encoder
+  """
+  #--------- Making the net -----------
+  # x_1 -> y_1 -> y_2 -> x_2
+  # this peice x_1 -> y_1
+  x_1 = inputs 
+ 
+  # normalize and formate the first layer
+  #keep_prob = tf.placeholder("float") # do a little dropout to normalize
+  x_1_norm = tf.nn.dropout(x_1, .8)
+  x_1_image = tf.reshape(x_1_norm, [-1, 28, 28, 1])
+  # conv1
+  conv1 = _conv_layer(x_1_image, 5, 1, 32, 1)
+  # conv2
+  conv2 = _conv_layer(conv1, 2, 2, 32, 2)
+  # conv3
+  conv3 = _conv_layer(conv2, 5, 1, 64, 3)
+  # conv4
+  conv4 = _conv_layer(conv3, 2, 2, 64, 4)
+  # fc5 
+  fc5 = _fc_layer(conv4, 512, 5, False, False)
+  # y_1 
+  y_1 = _fc_layer(conv4, 64, 6, True, False)
+
+  return y_1 
+
+def dynamic_compression(inputs):
+  """Builds compressed dynamical system part of the net.
+  Args:
+    inputs: input to system
+  """
+  #--------- Making the net -----------
+  # x_1 -> y_1 -> y_2 -> x_2
+  # this peice y_1 -> y_2
+  y_1 = inputs 
+ 
+  # (start indexing at 10) -- I will change this in a bit
+  # fc11
+  fc11 = _fc_layer(y_1, 512, 11, True, False)
+  # y_2 
+  y_2 = _fc_layer(fc11, 64, 11, True, False)
+
+  return fc5 
+
+def decoding(inputs):
+  """Builds decoding part of ring net.
+  Args:
+    inputs: input to decoder
+  """
+  #--------- Making the net -----------
+  # x_1 -> y_1 -> y_2 -> x_2
+  # this peice y_2 -> x_2
+  y_2 = inputs 
+ 
+  # fc21
+  fc21 = _fc_layer(y_2, 512, 21, True, False)
+  # fc23
+  fc22 = _fc_layer(fc21, 64*7*7, 22, True, False)
+  conv22 = tf.reshape(fc22, [-1, 7, 7, 64])
+  # conv23
+  conv23 = _transpose_conv_layer(conv22, 2, 2, 64, 23)
+  # conv24
+  conv24 = _transpose_conv_layer(conv23, 5, 1, 32, 24)
+  # conv25
+  conv25 = _transpose_conv_layer(conv24, 2, 2, 32, 25)
+  # conv26
+  conv26 = _transpose_conv_layer(conv25, 5, 1, 1, 26)
+  # x_2 
+  x_2 = tf.reshape(conv26, [-1, 784])
+
+  return x_2 
+
 def loss(output, correct_output):
-  error = tf.nn.l2_loss(x_2 - _x_2)
+  error = tf.nn.l2_loss(output - correct_output)
   return error
   
 def train(total_loss):
    lr = 1e-4
-   train_op = tf.trainAdamOptimizer(lr).minimize(total_loss)
+   train_op = tf.train.AdamOptimizer(lr).minimize(total_loss)
    return train_op
 
 '''
--- ring_net_train.py
+++ ring_net_train.py
@@ -6,20 +6,30 @@ import cannon as cn
 
 import ring_net
 
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer('max_steps', 100000,
+                            """number of batches to run""")
+
 def train():
   """Train ring_net for a number of steps."""
   with tf.Graph().as_default():
     # Get images and labels for CIFAR-10.
     k = cn.Cannon()
-    x_inputs = ring_net.inputs() 
+    x_1 = tf.placeholder(tf.float32, [None, 784])
+    x_2 = tf.placeholder(tf.float32, [None, 784])
+    # ring_net.inputs() 
 
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    output = ring_net.inference(x_inputs)
+    # encoding
+    y_1 = ring_net.encoding(x_1)
+    # dynamic system
+    y_2 = ring_net.dynamic_compression(y_1)
+    # decoding 
+    x_2_out = ring_net.decoding(y_2)
 
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
-    error = ring_net.loss(output)
+    error = ring_net.loss(x_2, x_2_out)
     train_op = ring_net.train(error)
 
     # List of all Variables
@@ -29,44 +39,38 @@ def train():
     init = tf.initialize_all_variables()
 
     # Start running operations on the Graph.
-    sess = tf.Session(config=tf.ConfigProto(
-        log_device_placement=FLAGS.log_device_placement))
+    sess = tf.Session()
     sess.run(init)
 
     # Start the queue runners.
-    tf.train.start_queue_runners(sess=sess)
+    #tf.train.start_queue_runners(sess=sess)
 
-    graph_def = sess.graph.as_graph_def(add_shapes=True)
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
-                                            graph_def=graph_def)
+    #graph_def = sess.graph.as_graph_def(add_shapes=True)
+    #summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+    #                                        graph_def=graph_def)
 
     for step in xrange(FLAGS.max_steps):
-      start_time = time.time()
-      learning_rate = neovision.RATES[bisect.bisect(neovision.CUM_ITERS_AT_RATE,step)]
-      _ , loss_value = sess.run([train_op, loss],feed_dict={keep_prob:FLAGS.dropout_hidden,keep_prob_input:dropout_input,lr:learning_rate})
-#      ret_value, df_dx_value, loss_value = sess.run([ret, df_dx, loss])
-      duration = time.time() - start_time
+      ins, outs = k.generate_28x28(1,50)
+      _ , loss_value = sess.run([train_op, error],feed_dict={x_inputs:ins[0], x_outputs:ins[0]})
+      print(loss_value)
 
       assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+      if step%20 == 0:
+        #new_im = x_outputs.eval(feed_dict={x_inputs: ins[0,10:11], x_outputs: ins[0,10:11]})
+        #for j in range(15):
+        #  new_im = y_conv.eval(feed_dict={x: new_im, keep_prob: 1.0})
+        #plt.imshow(new_im.reshape((28,28)))
+        #plt.savefig('new_run_1.png')
+        print("Saved")
 
-      if step % 10 == 0:
-        num_examples_per_step = FLAGS.batch_size
-        examples_per_sec = num_examples_per_step / duration
-        sec_per_batch = float(duration)
-
-        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                      'sec/batch)')
-        print (format_str % (datetime.now(), step, loss_value,
-                             examples_per_sec, sec_per_batch))
-
-      if step % 100 == 0:
-        summary_str = sess.run(summary_op,feed_dict={keep_prob:FLAGS.dropout_hidden,keep_prob_input:FLAGS.dropout_input,lr:learning_rate})
-        summary_writer.add_summary(summary_str, step)
+      #if step % 100 == 0:
+      #  summary_str = sess.run(summary_op,feed_dict={keep_prob:FLAGS.dropout_hidden,keep_prob_input:FLAGS.dropout_input,lr:learning_rate})
+      #  summary_writer.add_summary(summary_str, step)
 
       # Save the model checkpoint periodically.
-      if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-        checkpoint_path = os.path.join(FLAGS.train_dir, 'YoloNeovision.ckpt')
-        saver.save(sess, checkpoint_path, global_step=step)
+      #if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+      #  checkpoint_path = os.path.join(FLAGS.train_dir, 'YoloNeovision.ckpt')
+      #  saver.save(sess, checkpoint_path, global_step=step)
 
 def main(argv=None):  # pylint: disable=unused-argument
   train()
