5a9f4655b1049988d13e06850683b79a0ee977ad
hennigo <hennigo@viki.ai>
2016-06-08 18:28:54 -0400

    making tf records now

--- utils/createTFGoldfish.py
+++ utils/createTFGoldfish.py
@@ -4,49 +4,58 @@ import numpy as np
 import tensorflow as tf 
 import cv2 
 # name of goldfish data 
-cap = cv2.VideoCapture("test.webm") 
+video_file = "goldfish.webm"
+cap = cv2.VideoCapture(video_file) 
+
+# seq length
+SEQ_LENGTH = 2
+
 # number of frames per data point 
 FRAMES_NUM = 4 
+
 # resize shape 
-SHAPE = (78,78)
+SHAPE = (28,28)
 
 # create tf writer
-writer = tf.python_io.TFRecordWriter("goldfish.tfrecords")
+record_filename = "goldfish_28x28_" + str(SEQ_LENGTH) + ".tfrecords"
+writer = tf.python_io.TFRecordWriter(record_filename)
 
 # the stored frames
-frames = np.zeros((SHAPE[0], SHAPE[1], FRAMES_NUM * 3))
+frames = np.zeros((SHAPE[0], SHAPE[1], FRAMES_NUM))
 
 # helper function
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-while(True):
+# num frames
+ind = 0
+
+# end of file
+end = False 
+
+while(not end):
   # create frames
   for i in xrange(FRAMES_NUM):
     ret, frame = cap.read()
     if not ret:
-      print("starting over")
-      cap.release()
-      cap = cv2.VideoCapture("test.webm")
-      ret, frame = cap.read()
-      print(ret)
+      end = True 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.resize(frame, SHAPE, interpolation = cv2.INTER_CUBIC)
-    frames[:, :, i:i+3] = frame 
+    frames[:, :, i] = frame/255.0
   # process frame for saving
-  frames_flat = frames.reshape([1,SHAPE[0]*SHAPE[1]*FRAMES_NUM*3])
-    
-   
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  print(frame.shape)
-
-  # Our operations on the frame come here
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  frames_flat = frames.reshape([1,SHAPE[0]*SHAPE[1]*FRAMES_NUM])
+  frame_raw = frames_flat.tostring()
+  # create example and write it
+  example = tf.train.Example(features=tf.train.Features(feature={
+    'image': _bytes_feature(frame_raw)})) 
+  writer.write(example.SerializeToString()) 
 
   # Display the resulting frame
-  cv2.imshow('frame',gray)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
+  #cv2.imshow('frame',frames[:,:,0])
+  #if cv2.waitKey(1) & 0xFF == ord('q'):
+  #    break
+  ind = ind + 1
+  print(ind)
 
 # When everything done, release the capture
 cap.release()
