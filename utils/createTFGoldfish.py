45664818fac0882bc94d5e303d97eaabe7899648
Oliver Hennigh <hennigo@hennigo.3tc1k178.rl.af.mil>
2016-06-07 20:51:25 -0400

    this is on hold now. I will just load video while training for now

--- utils/createTFGoldfish.py
+++ utils/createTFGoldfish.py
@@ -9,33 +9,40 @@ cap = cv2.VideoCapture("goldfish.webm")
 # number of frames per data point
 FRAMES_NUM = 4
 
+# resize shape
+SHAPE = (78,78)
+
 # create tf writer
 writer = tf.python_io.TFRecordWriter("goldfish.tfrecords")
 
 # the stored frames
-frames = []
+frames = np.zeros((SHAPE[0], SHAPE[1], FRAMES_NUM * 3))
 
+# helper function
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-for i in xrange(FRAMES_NUM):
-  ret, frame = cap.read()
-  frames.append(frame)
-   
-
 while(True):
-
+  # create frames
+  for i in xrange(FRAMES_NUM):
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, SHAPE, interpolation = cv2.INTER_CUBIC)
+    frames[:, :, i:i+3] = frame 
+  # process frame for saving
+  frames_flat = frames.reshape([1,SHAPE[0]*SHAPE[1]*FRAMES_NUM*3])
     
+   
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  print(frame.shape)
 
-    # Capture frame-by-frame
+  # Our operations on the frame come here
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+  # Display the resulting frame
+  cv2.imshow('frame',gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
 
 # When everything done, release the capture
 cap.release()
